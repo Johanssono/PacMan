48668d5b51d2fd632167fad1a7324ab97712d43c
Karlsson0214 <karlsson0214@gmail.com>
2024-10-08 14:51:53 +0200

    Better movement, rotation and collision detection

--- Turtle.cs
+++ Turtle.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace WallsStart
 {
@@ -18,12 +19,17 @@ namespace WallsStart
         private static Texture2D texture;
         // Containts thes direction the turtle is facing.
         private SpriteEffects spriteEffect;
-        // The center of the turtel.
+        // The center of the turtle.
         private Vector2 position;
         private float speed;
-        private const int RIGHT = 1;
-        private const int LEFT = 2;
-        private int lastDirection = RIGHT;
+        // rotation in degrees
+        // 0 = right, 90 = down, 180 = left, 270 = up
+        private static int RIGHT = 0;
+        private static int DOWN = 90;
+        private static int LEFT = 180;
+        private static int UP = 270;
+        // TODO lock rotation to 0 - 359.999... degrees
+        private float rotation = 0f;
         private IWorld world;
 
 
@@ -73,37 +79,57 @@ namespace WallsStart
         /// <param name="gameTime">A GameTime object that represents the time in the game.</param>
         public void Update(GameTime gameTime)
         {
-            // Move the turtle based on input.
-            var keyState = Keyboard.GetState();
-            if (keyState.IsKeyDown(Keys.Left))
+            // Move the turtle based on input
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left))
             {
-                // If the turtle face a new direction, flip it.
-                if (lastDirection == RIGHT)
-                {
-                    spriteEffect = SpriteEffects.FlipHorizontally;
-                    lastDirection = LEFT;
-                }
+                rotation = LEFT;
                 position.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
-            if (keyState.IsKeyDown(Keys.Right))
+            else if (keyboardState.IsKeyDown(Keys.Right))
             {
-                // If the turtle face a new direction, flip it.
-                if (lastDirection == LEFT)
-                {
-                    spriteEffect = SpriteEffects.None;
-                    lastDirection = RIGHT;
-                }
+                rotation = RIGHT;
                 position.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             }
+            // TODO
+            // Write your code here.
+
+
+
             // Check for collision with walls.
             foreach (Wall wall in world.GetWalls())
             {
                 if (CollidesWith(wall))
                 {
-                    // Reset the position of the turtle.
-                    Position = new Vector2(world.Width / 2, world.Height / 2);
+                    // If turtle is colliding with a wall. Move back.
+                    if (HasRotation(RIGHT))
+                    {
+                        // move back
+                        position.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    }
+                    else if (HasRotation(LEFT))
+                    {
+                        // move back
+                        position.X += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    }
+                    // TODO
+                    // Write your code here.
+
+
+
                 }
             }
+        }
+        /// <summary>
+        /// Check if the turtle has a rotation.
+        /// </summary>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        public bool HasRotation(float rotation)
+        {
+            float deltaRotation = 0.1f;
+            return rotation - deltaRotation < this.rotation && this.rotation < rotation + deltaRotation;
         }
         /// <summary>
         /// Call once per frame to draw the turtle to the screen.
@@ -113,14 +139,14 @@ namespace WallsStart
         {
             // Draw the turtle, centered on the position.
             spriteBatch.Draw(texture,
-                position,
-                null,
-                Color.White,
-                0f,
-                new Vector2(texture.Width / 2, texture.Height / 2),
-                Vector2.One,
-                spriteEffect,
-                0f);
+               position,
+               null,
+               Color.White,
+               rotation * MathF.PI / 180f, // Convert to radians
+               new Vector2(texture.Width / 2, texture.Height / 2), // Center image on position
+               Vector2.One,
+               SpriteEffects.None,
+               0f);
         }
         /// <summary>
         /// Returns true if the turtle rectangle overlaps with the walls rectangle.
@@ -137,13 +163,31 @@ namespace WallsStart
             float turtleRight = position.X + texture.Width / 2;
             float turtleTop = position.Y - texture.Height / 2;
             float turtleBottom = position.Y + texture.Height / 2;
+            if (HasRotation(UP) || HasRotation(DOWN))
+            {
+                // When turtle rotated 90 degrees
+                // swap width and height.
+                turtleLeft = position.X - texture.Height / 2;
+                turtleRight = position.X + texture.Height / 2;
+                turtleTop = position.Y - texture.Width / 2;
+                turtleBottom = position.Y + texture.Width / 2;
+            }
             float wallLeft = wall.Position.X - wall.Width / 2;
             float wallRight = wall.Position.X + wall.Width / 2;
             float wallTop = wall.Position.Y - wall.Height / 2;
             float wallBottom = wall.Position.Y + wall.Height / 2;
+            // a corner of turtle is inside of wall
             if (IsBetween(turtleLeft, wallLeft, wallRight) || IsBetween(turtleRight, wallLeft, wallRight))
             {
                 if (IsBetween(turtleTop, wallTop, wallBottom) || IsBetween(turtleBottom, wallTop, wallBottom))
+                {
+                    return true;
+                }
+            }
+            // a corner of wall is inside of turtle
+            if (IsBetween(wallLeft, turtleLeft, turtleRight) || IsBetween(wallRight, turtleLeft, turtleRight))
+            {
+                if (IsBetween(wallTop, turtleTop, turtleBottom) || IsBetween(wallBottom, turtleTop, turtleBottom))
                 {
                     return true;
                 }
